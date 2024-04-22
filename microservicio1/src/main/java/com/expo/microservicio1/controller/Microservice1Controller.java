c597dd43256e09914a208290bf11e87ef8d209e8
luisvill02 <lvillamil@unimagdalena.edu.co>
2024-04-22 14:06:23 -0500

    added endpoints

--- microservicio1/src/main/java/com/expo/microservicio1/controller/Microservice1Controller.java
+++ microservicio1/src/main/java/com/expo/microservicio1/controller/Microservice1Controller.java
@@ -1,9 +1,7 @@
 package com.expo.microservicio1.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/microservicio1")
@@ -12,5 +10,20 @@ public class Microservice1Controller {
     @GetMapping
     public ResponseEntity<String> getMicroservice2() {
         return ResponseEntity.ok("Hello from Microservice 1");
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> postMicroservice2(@RequestBody Object algo) {
+        return ResponseEntity.ok("Hello " + algo.toString() + " from Microservice 1");
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<String> getMicroservice2(@PathVariable String name) {
+        return ResponseEntity.ok("Hello " + name + " from Microservice 1");
+    }
+
+    @GetMapping("/param")
+    public ResponseEntity<String> getMicroservice2Param(@RequestParam String name) {
+        return ResponseEntity.ok("Hello " + name + " from Microservice 1");
     }
 }
